905947123c87737ab59e21b2dd227f30522138cf
Jeremy Breese <jbreese99@gmail.com>
2019-08-26 21:51:21 -0500

    Adding challenge 1 and 2 to this repo

---
+++ Challenge1/fizzBuzz.java
@@ -0,0 +1,17 @@
+class fizzBuzz {
+  public static void main(String args[]) {
+    for (int i = 1; i < 101; i++) {
+
+      if ((i % 3 == 0)||(i % 5 == 0)){
+        if (i % 3 == 0)
+        System.out.print("Fizz");
+        if (i % 5 == 0)
+        System.out.print("Buzz");
+        System.out.println();
+        continue;
+      }
+
+      System.out.println(i);
+    }
+  }
+}
