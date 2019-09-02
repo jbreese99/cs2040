2e4b6fa8cb0861689fe9fbc712a58ce49ee75dd9
Jeremy Breese <jbreese99@gmail.com>
2019-09-01 20:19:06 -0500

    Add challenge 3 to repo

---
+++ Challenge3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Challenge3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String docName;
+            String contents;
+
+            Console.WriteLine("Document \n");
+
+            Console.WriteLine("Enter the name of the document here:\n");
+            docName = Console.ReadLine() + ".txt";
+
+            Console.WriteLine("Enter the contents of the documet here: \n");
+            contents =Console.ReadLine();
+
+            System.IO.File.WriteAllText(docName, contents);
+
+            try {
+                System.IO.File.WriteAllText(docName, contents);
+            } catch (Exception ex) {
+                Console.WriteLine(ex);
+            } 
+
+             Console.WriteLine(docName + " was successfully saved The document contains " + contents.Length + " characters" );
+        }
+    }
+}
